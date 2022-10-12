4f41c944dc1432bd89b7ce9e40d6516b33796117
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-10-13 02:21:06 +0900

    ノードベースのフロー描画ライブラリのReact Flowを入れて見て、ノードや線を表示してみた

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Background, Controls, Edge, Node, ReactFlow } from "reactflow";
+import "reactflow/dist/style.css";
+
+const nodes: Node[] = [
+  {
+    id: "1",
+    data: { label: "Node 1" },
+    position: { x: 0, y: 0 },
+  },
+  {
+    id: "2",
+    data: { label: "Node 2" },
+    position: { x: 100, y: 100 },
+  },
+];
+
+const edges: Edge[] = [
+  {
+    id: "1-2",
+    source: "1",
+    target: "2",
+    label: "to the",
+    type: "step",
+  },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: "100vh" }}>
+      <ReactFlow
+        // フローエディタに表示して制御するノードを渡す
+        nodes={nodes}
+        // フローエディタにあるノード間をつなぐ線を渡す
+        edges={edges}
+      >
+        {/* フローエディターのドット方眼背景を表示するコンポーネント */}
+        <Background />
+        {/* フローエディターを拡大、縮小、サイズ調整、ロック制御などをしたりするコンポーネント */}
+        <Controls />
+      </ReactFlow>
     </div>
   );
 }
